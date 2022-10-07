05a636ed4fb5d453e60d8df9e980e7d4ec64408b
Violets-puragtory <110205356+Violets-puragtory@users.noreply.github.com>
2022-10-06 22:21:15 -0500

    Update porn.js

--- porn.js
+++ porn.js
@@ -1,3 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <button id="baton" onclick="play()">DO NOT CLICK
+    </button>
+   
+    <img id="sex" width="100%" src="./Images/No sex.jpg"/>
+
+    <iframe id='kaboom' src="https://gifer.com/embed/2a9n" width="0%" frameBorder="0" allowFullScreen></iframe>
+
+</html>
+
+<style>
+    #sex {
+
+
+        animation: hello 2s forwards paused,
+    violent-rotation 0.25s 3s infinite paused linear;  
+    }
+
+    @keyframes violent-rotation {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+
+    @keyframes hello {
+        from{
+            width: 0%;
+        }
+        to {
+            width: 100%;
+        }
+    }
+</style>
+
+<script>
     
 function play() {
     var audio = new Audio('./Sounds/Penis Music.mp3');
@@ -20,15 +59,6 @@ function play() {
     document.getElementById('sex').remove()
     
     document.getElementById('kaboom').style.width = "100%"
-
-sleep(1500).then(() => {
-    document.getElementById('kaboom').remove()
-    txt = "a";
-while(1){
-    txt = txt += "a";
-}
-
-    })
 })
 
 
@@ -36,3 +66,6 @@ while(1){
     
 
 }   
+
+
+</script>
