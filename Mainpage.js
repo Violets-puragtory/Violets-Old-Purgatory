e3a88c59fbce4cc28af753144a89ad7fb5f836d2
Violets-puragtory <110205356+Violets-puragtory@users.noreply.github.com>
2022-10-13 13:52:42 -0500

    real

--- Mainpage.js
+++ Mainpage.js
@@ -2,5 +2,5 @@ function Help() {
     document.getElementById("Body").remove();
     let help = document.createElement("p");
     help.className = "much";
-    document.getElementById("death").appendChild(help)
+    document.getElementById("death").appendChild(help);
 }
--- Redirects/hackr/hacksr.js
+++ Redirects/hackr/hacksr.js
@@ -1,9 +1,8 @@
 
 const bowomp = new Audio("deltarune explosion greenscreen.mp3");
-
 bowomp.load()
 
 function UwUExplodesViolently() {
-    document.getElementById("body").remove()
     bowomp.play()
+    document.getElementById("body").remove()
 }
