a45d9a3db44164fc41e7ae2de90a0a4318af2c82
Violets-puragtory <110205356+Violets-puragtory@users.noreply.github.com>
2022-10-13 12:56:31 -0500

    a

--- Mainpage.js
+++ Mainpage.js
@@ -2,4 +2,5 @@ function Help() {
     document.getElementById("Body").remove();
     let help = document.createElement("p");
     help.className = "much";
+    document.getElementById("death").appendChild(help)
 }
