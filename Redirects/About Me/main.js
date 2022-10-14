a0a7258c8abd5e38a7e5b514f9e3e372fdc9ab47
Violets-puragtory <110205356+Violets-puragtory@users.noreply.github.com>
2022-10-14 08:55:57 -0500

    who

--- Redirects/About Me/main.js
+++ Redirects/About Me/main.js
@@ -1,3 +1,7 @@
-if (Math.floor(Math.random() * 2) == 1) {
+let gamer = Math.floor(Math.random() * 2)
+
+
+
+if (gamer == 1) {
     document.getElementById("Who").textContent = "Who the fuck am I??"
 }
