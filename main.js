871bfd859743c277cc35bfc114284224f5798cee
Violets-puragtory <Violetbutagain@gmail.com>
2022-10-25 23:37:44 -0500

    Added bootstrap added glitch effect added about me

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 function makeid(length) {
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -10,22 +12,49 @@ function makeid(length) {
 
 
 function aboutme() {
+
+    $(".button").prop("disabled",true);
+
     const text = $("#buttontext");
-    text.css("font-family", "Rubik Glitch")
-    text.css("whitespace", "wrap")
+    text.css("font-family", "Rubik Glitch");
+    text.css("whitespace", "wrap");
     
-    newtext  =  "The Fitness Gram Pacer Test"
+    newtext  =  "My name is Violet, I am a young-ish mediocre (at best) 'developer'. I hope to one day be a game developer, but as of now it doesnt seem that way is going to go well. I've been learning/learnt a few languages, most being quite simple. A list can be found below. (Up to date as of October 25th 2022)"
+
     text.css("width",  "100%")
     
-    for (var i = 0, len = 20; i < len; i++) {
+    width = $( window ).width() / 80;
+
+
+
+    for (var i = 0, len = 25; i < len; i++) {
+        const x = i
         setTimeout(() => {
-            text.html(makeid(i) + '<br>' + makeid(i) + '<br>'  + makeid(i)) 
-        }, i * 25);
+            var glitch = makeid((width / 10) * x) + '<br>'
+            var fullglitch = glitch
+            
+            for (var i = 0, len = 10; i < len; i++) {
+                glitch = makeid((width / 10) * x) + '<br>'
+                fullglitch = fullglitch + glitch
+            }
+
+            text.html(fullglitch) 
+        }, i * 20);
     };
     setTimeout(() => {
-        text.html('newtext')
+        text.html(newtext)
         text.css("font-family",  "Rubik, Verdana")
+        text.css("font-size", "1.5em")
+        $(".button").css("border-color", "white")
+        $("a").attr("target", "_blank");
+        $(".baller > ul > *").css("font-size", "0.7em")
+        $(".baller > .fakeheader").css("font-size", "2em")
     }, 550);
+
+    setTimeout(() => {
+        $("#aboutme").css("border-color", "blueviolet")
+        $(".button").prop("disabled",false);
+    }, 600);
     
 }
 
