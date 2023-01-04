e8b780fc987867826e03610e5d75d35ade2be498
Violets-puragtory <110205356+Violets-puragtory@users.noreply.github.com>
2023-01-04 16:24:56 +0000

    asd

--- SubSites/DrivingGame/main.js
+++ SubSites/DrivingGame/main.js
@@ -1,36 +1,34 @@
 let WPressed = false;
 let Y = 0
+let X = 0
+let bonusveloc = 0
 
 
-function drive(numbaY) {
-    $("#car").css("transform", `translateY(${Y + "px"})`)
-    if(numbaY < 2 && numbaY > -2) {
-        Y = Y + numbaY
-    }
+function drive(speed) {
+
+    let numbaY = speed
+    let numbaX = speed
+
+    Y = Y + Math.min(Math.max(numbaY / 5, -1.5), 2)
+    X = X + Math.min(Math.max(numbaX / 5, -1.25), 1.5)
+    $("#car").css("transform", `translateY(${Y + "%"}) translateX(${X + "%"})`)
+    
+
+
     setTimeout(() => {
-        if(WPressed == true) {
-            drive(numbaY * 1)
-        } 
+        if (WPressed == true) {
+            drive(numbaY * 1.1, numbaX * 1.1)
+        }
     }, 1);
 }
 
-function drive(amount) {
-    $("#car").css("transform", `translateY(${Y + "px"})`)
-    if(numbaY < 2 && numbaY > -2) {
-        Y = Y + numbaY
-    }
-    setTimeout(() => {
-        if(WPressed == true) {
-            drive(numbaY * 1)
-        } 
-    }, 1);
-}
+
+
 
 $(window).keydown(function (evt) {
-    if (evt.which == 87) {
+    if (evt.which == 87 && WPressed == false) {
         WPressed = true;
-        $("#car").text("W")
-        drive(1)
+        drive(1, 1)
     }
 }).keyup(function (evt) {
     if (evt.which == 87) {
@@ -38,10 +36,13 @@ $(window).keydown(function (evt) {
         $("#car").text("")
     }
 }).keydown(function (evt) {
-    if (evt.which == 83) {
-        WPressed = true;
-        $("#car").text("S")
-        drive(-1)
+    if (evt.which == 83 && WPressed == false) {
+        WPressed = false;
+        setTimeout(() => {
+            WPressed = true;
+            $("#car").text("S")
+            drive(-1, -1)
+        }, 3);
     }
 }).keyup(function (evt) {
     if (evt.which == 83) {
@@ -49,7 +50,7 @@ $(window).keydown(function (evt) {
         $("#car").text("")
     }
 }).keydown(function (evt) {
-    if (evt.which == 65) {
+    if (evt.which == 65 && WPressed == false) {
         $("#car").text("A")
         carturn()
     }
